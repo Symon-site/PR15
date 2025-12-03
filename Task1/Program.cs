18e674e1e1c429fa03e68173e321c4f49090c897
Lololoshka <garkinsymon81@gmail.com>
2025-12-03 11:24:21 +0300

    Task1 added

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Robot robot = new Robot("МшкФредди");
+            robot.Work();
+            robot.Work();
+            robot.Charger();
+            robot.Work();
+        }
+    }
+
+}
+
---
+++ Task1/Robot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class Robot: Worker, Charger
+    {
+        public int EnergyLevel { get; set; }
+        public string Name { get; set; }
+        public Robot(string name)
+        {
+            Name = name;
+            EnergyLevel = 100;
+        }
+        public void Work()
+        {
+            if (EnergyLevel > 0)
+            {
+                EnergyLevel = Math.Max(0, EnergyLevel - 20);
+                Console.WriteLine($"{Name} функционирует; текущая энергия: {EnergyLevel}");
+
+            }
+            else
+            {
+                Console.WriteLine($"{Name} разряжен");
+            }
+        }
+        public void Charger()
+        {
+            EnergyLevel = 100;
+            Console.WriteLine($"{Name} заряжен");
+        }
+    }
+}
