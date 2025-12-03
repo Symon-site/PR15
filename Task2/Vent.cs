b3f2e790ec8ac2c7a8a32301a851772d3b22536c
Gar’kin Semyon <garkinsymon81@gmail.com>
2025-12-03 11:51:19 +0300

    edited

--- Task2/Vent.cs
+++ Task2/Vent.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp32
 {
-    internal class Vent
+    internal class Vent: Turnable
     {
+    
+    public bool IsOn { get; private set; }
+
+    public void TurnOff()
+    {
+        IsOn = false;
+        Console.WriteLine("Вентилятор выключен!");
+    }
+
+    public void TurnOn()
+    {
+        IsOn = true;
+        Console.WriteLine("Вентилятор включен!");
+    }
+
+    public void ShowState()
+    {
+        Console.WriteLine($"Вентилятор: {(IsOn ? "Включен" : "Выключен")}");
+    }
+    
     }
 }
