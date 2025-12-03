155fc66ad90d9098f98064ecda52ade495f2d794
Gar’kin Semyon <garkinsymon81@gmail.com>
2025-12-03 11:48:48 +0300

    edited

--- Task2/Lamp.cs
+++ Task2/Lamp.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp32
 {
-    internal class Lamp
+    internal class Lamp: Turnable, LevelAdaptable
     {
+            private bool isOn;
+    private int level;
+    public bool IsOn => isOn;
+    public int Level => level;
+
+    public Lamp()
+    {
+        isOn = false;
+        level = 0;
     }
+
+    public void TurnOn()
+    {
+        isOn = true;
+        level = 100;
+        Console.WriteLine("Лампа включена");
+    }
+
+    public void TurnOff()
+    {
+        isOn = false;
+        level = 0;
+        Console.WriteLine("Лампа выключена");
+    }
+
+    public void SetLevel(int level)
+    {
+        level = level < 0 ? 0 : level > 100 ? 100 : level;
+        Console.WriteLine($"Яркость лампы: {level}%");
+    }
+
+    public void ShowState()
+    {
+        Console.WriteLine($"Лампа: {(isOn ? "Включена" : "Выключена")}; уровень: {level}%");
+    }
+}
+    
 }
